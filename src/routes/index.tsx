45232757ebb5dc158e7673bab18b85fec963f40b
John Johnson <jjmbeba@gmail.com>
2026-01-02 18:06:51 +0300

    feat: install shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "../components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -8,6 +9,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3 className="text-lg text-red-500">Welcome Home!</h3>
+      <Button>Welcome home</Button>
     </div>
   );
 }
