d1a0fc6fd91b14e8b9c41de2162f0573046a83ee
John Johnson <jjmbeba@gmail.com>
2026-01-02 17:30:00 +0300

    feat: install ultracite

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+<<<<<<< Updated upstream
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" rel="noopener" target="_blank">
+					<img alt="Vite logo" className="logo" src={viteLogo} />
+				</a>
+				<a href="https://react.dev" rel="noopener" target="_blank">
+					<img alt="React logo" className="logo react" src={reactLogo} />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
+=======
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vite.dev" rel="noopener" target="_blank">
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" rel="noopener" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +57,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
+>>>>>>> Stashed changes
 }
 
-export default App
+export default App;
